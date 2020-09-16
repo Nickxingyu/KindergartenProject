aea87b12b94d9f156aadb614006fa236b8664372
XuXingyu <nick19960723@gmai..com>
2020-09-16 09:56:05 +0800

    Set helmet to protect the server

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const helmet = require("helmet");
 
+const config = require("./config.json");
 const location = require("./routes/location");
 
 var app = express();
-var port = 3000;
+var port = config.server.port;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 app.use("/location",location);
+app.use(helmet());
 app.listen(port, function () {
   console.log("Example app listening at http://localhost:".concat(port));
 });

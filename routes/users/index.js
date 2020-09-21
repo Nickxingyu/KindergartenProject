8adb41b5cf75cb11f48eb1d984c953644ed003b7
XuXingyu <nick19960723@gmai..com>
2020-09-22 01:06:59 +0800

    Add api 'loginByPassword'

--- routes/users/index.js
+++ routes/users/index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userAuth = require("../../controllers/userAuth");
 
-router.use("/testPWD",(req,res,next)=>{
-    const phone = "0985895611";
-    const pwd = "2307199685Nick";
-    userAuth.loginByPassword({phone:phone, password: pwd},(err,user,info)=>{
+router.post("/loginByPassword",(req, res, next)=>{
+    const {phone, password} = req.body;
+    userAuth.login_by_password({phone:phone, password: password},(err,user,info)=>{
         if (err) return next(err);
         if (!user) return res.status(401).json(info);
         res.json(info.body);

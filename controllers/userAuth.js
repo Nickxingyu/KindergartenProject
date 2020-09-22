8e5564d836d3e4f1e1f10b11b74773f2b9930d73
XuXingyu <nick19960723@gmai..com>
2020-09-22 21:16:22 +0800

    finish the interface of login

--- controllers/userAuth.js
+++ controllers/userAuth.js
@@ -1,74 +1,29 @@
 const User = require('../models/database/mongo/DataBase/user');
+const verification_service = require('../helpers/verification_service');
 const login_message = require('../models/enum/msg_enum').login_message;
 
 module.exports = {
-    login_by_password: (userInfo, callback)=>{
-        const { phone, email, password } = userInfo;
-        let condition = {};
-        if ( !phone && !email ){
-            return callback(null, false, login_message.content_not_complete)
-        }
-        condition = phone ? {"user.phone": phone} : {"user.email":email}
-        User.findOne(condition, (err,user)=>{
+    login_by_password: (userInfo, callback) => {
+        const {phone, password} = userInfo;
+        if (!phone) return callback(null, false, login_message.content_not_complete)
+        User.findOne({"user.phone": phone}, (err,user)=>{
             if (err)
                 return callback(err);
             if (!user)
                 return callback(null, false, login_message.no_user_founded);
             if (!user.validPassword(password))
                 return callback(null, false, login_message.wrong_password);
-            /*
-             *
-             * add code to build JWT Token with api key
-             * 
-             */
+            const token = build_token(phone);
             return callback(null, user, {
-                /*
-                 * add " headers: { Authorization: results.token } "
-                 */
+                headers: { Authorization: token},
                 body:login_message.authentication_succeeded
             })
         })
     },
-    login_by_verifiction_code: (userInfo, callback) => {
-        const {phone, user_verification_code} = userInfo;
-        if(!phone){
-            return callback(null, false, login_message.content_not_complete)
-        }
-        const compare_result = check_verification_code(phone, user_verification_code);
-        if(!compare_result){
-            return callback(null, false, login_message.invalid_verification_code)
-        }else{
-            User.findOne({"user.phone": phone}, (err, user) => {
-                if(err) 
-                    return callback(err);
-                if(!user) 
-                    return callback(null, user, login_message.no_user_founded)
-                /*
-                 *
-                 * add code to build JWT Token with api key
-                 * 
-                 */
-                return callback(null, user, {
-                    /*
-                     * add " headers: { Authorization: results.token } "
-                     */
-                    body: login_message.authentication_succeeded
-                })
-            })
-        } 
-    },
-    send_verification_code: (phone) => {
-
-    },
-    //check_verification_code: 
+    login_by_verification_code: (userInfo, callback) => {
+        const {phone, token} = userInfo;
+        if(!phone) return callback(null, false, login_message.content_not_complete)
+        if(!token) return verification_service.send_verifiaction_code(phone, callback)
+        return verification_service.check_verification_code_for_JWT(phone, token,callback)
+    }
 };
-
-function check_verification_code(phone, user_verification_code) {
-        /*  redis get server-side verification code
-         *  compare verification codes    
-         */
-}
-
-function get_api_key(phone){ 
-
-}

454c65b1f6e112720be964112e95d94903b21817
XuXingyu <nick19960723@gmai..com>
2020-09-22 21:19:47 +0800

    add verification message

--- models/enum/msg_enum.js
+++ models/enum/msg_enum.js
@@ -8,6 +8,7 @@ function msg_type_generator(type){
 }
 
 const login_message = msg_type_generator('login_message');
+const verification_code_message = msg_type_generator('verification_code_message');
 
 module.exports = {
     login_message:{
@@ -16,6 +17,11 @@ module.exports = {
         wrong_password: login_message('Wrong password'),
         authentication_succeeded: login_message('Authentication succeeded'),
         invalid_verification_code: login_message('Invalid verification code')
+    },
+    verification_code_message:{
+        no_user_founded: verification_code_message('No user founded'),
+        invalid_verification_code: verification_code_message('Invalid verification code'),
+        expired_verification_code: verification_code_message('Expired verification code')
     }
 }
 

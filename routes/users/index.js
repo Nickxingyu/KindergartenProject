1c5229d8c4fd228bee63e2d6f98d8a34b1fc1055
XuXingyu <nick19960723@gmai..com>
2020-09-24 17:17:45 +0800

    add two api

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,13 +1,51 @@
 const express = require('express');
 const router = express.Router();
 const userAuth = require("../../controllers/userAuth");
+const userKey = require('../../models/database/mongo/DataBase/userKey');
+const key = require('../../config/key');
 
 router.post("/loginByPassword",(req, res, next)=>{
-    const {phone, password} = req.body;
-    userAuth.login_by_password({phone:phone, password: password},(err,user,info)=>{
-        if (err) return next(err);
-        if (!user) return res.status(401).json(info);
-        res.json(info.body);
+    const {phone, password, publicKey} = req.body;
+    userAuth.login_by_password(
+        {
+            phone, 
+            password,
+            publicKey
+        },
+        (err, result, info)=>{
+        if (err) next(err);
+        else if (!result) res.status(401).json(info);
+        else if(!info.headers) res.json(info);
+        else{
+            res.header({ Authorization: info.headers.Authorization });
+            res.json(info.body);
+        }
+    })
+})
+
+router.post("/loginByVerificationCode",(req, res, next)=>{
+    const {phone} = req.body;
+    const authorization = req.header('Authorization');
+    const token = authorization ? authorization.replace('Bearer ', '') : null;
+    userAuth.login_by_verification_code(
+        { phone, token},
+        (err, result, info)=>{
+            if(err) next(err);
+            else if(!result) res.status(401).json(info);
+            else if(!info.headers) res.json(info);
+            else{
+                res.header({ Authorization: info.headers.Authorization });
+                res.json(info.body);
+            }
+    })
+})
+
+router.post("/getVerificationCode",(req, res, next)=>{
+    const {phone} = req.body;
+    userAuth.send_verification_code(phone,(err, user, info)=>{
+        if(err) next(err)
+        if(!user) res.status(401).json(info);
+        res.json(info);
     })
 })
 

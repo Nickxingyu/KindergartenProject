d7e768d374d82628f93dd9851361cafb058a01b4
XuXingyu <nick19960723@gmai..com>
2020-11-10 21:25:05 +0800

    change login process

--- helpers/AWS/SNS.js
+++ helpers/AWS/SNS.js
@@ -5,7 +5,7 @@ AWS.config = AWS_config;
 var sns = new AWS.SNS();
 sns.setSMSAttributes({attributes:{DefaultSMSType: 'Transactional'}}, function(err, data) {
     if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
+    // successful response
   })
 
 module.exports = {
